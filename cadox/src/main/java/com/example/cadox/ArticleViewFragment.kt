7fe3869e89ad88e8cd477c258af2a396d8ea1bb9
riderofzion <quentinrouet@hotmail.fr>
2023-03-15 17:02:27 +0100

    Mod 5 Navigation

---
+++ cadox/src/main/java/com/example/cadox/ArticleViewFragment.kt
@@ -0,0 +1,44 @@
+package com.example.cadox
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cadox.databinding.FragmentArticleViewBinding
+import com.example.cadox.repository.ArticleRepository
+import com.google.android.material.snackbar.Snackbar
+import java.util.*
+
+
+class ArticleViewFragment : Fragment() {
+    lateinit var favb : FragmentArticleViewBinding
+    //retourne la vue à afficher
+    // On a un fichier Layout XML qu'on doit transformer en vue pour l'afficher
+    //Et on doit aussi récupérer une instance binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        favb = FragmentArticleViewBinding.inflate(inflater, container, false)
+        return favb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        favb.article = ArticleRepository.getArticle(1)
+        if(favb.article != null){
+            favb.textViewDate.text = if(favb.article?.dateAchat == null){
+                Date(1678877335000L).toString()
+            }else{
+                favb.article!!.dateAchat.toString()
+            }
+            favb.imageButtonUrl.setOnClickListener {
+                Snackbar.make(it,
+                    favb.article?.url ?: "",
+                    1000).show()
+            }
+        }
+    }
+}
---
+++ mod5navigation/src/main/java/com/example/mod5navigation/ButtonFragment.kt
@@ -0,0 +1,29 @@
+package com.example.mod5navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class ButtonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_button, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //On récupère le button et lors du clic sur celui-ci on navigue vers TargetFragment
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            findNavController().navigate(R.id.action_buttonFragment_to_targetFragment)
+        }
+    }
+}
