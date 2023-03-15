6dfa25543df06448343d0db95c84e4d1aedf5d5e
riderofzion <quentinrouet@hotmail.fr>
2023-03-15 11:56:32 +0100

    correction databinding

--- cadox/src/main/java/com/example/cadox/ArticleViewActivity.kt
+++ cadox/src/main/java/com/example/cadox/ArticleViewActivity.kt
@@ -6,34 +6,32 @@ import android.widget.CheckBox
 import android.widget.ImageButton
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.cadox.databinding.ActivityArticleViewBinding
 import com.example.cadox.repository.ArticleRepository
 import com.google.android.material.snackbar.Snackbar
+import java.time.Instant
+import java.time.LocalDate
+import java.util.Date
 
 class ArticleViewActivity : AppCompatActivity() {
+    lateinit var aavb : ActivityArticleViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article_view)
-        val textViewDate = findViewById<TextView>(R.id.textViewDate)
-        val textViewPrice = findViewById<TextView>(R.id.textViewPrice)
-        val textViewUrl = findViewById<TextView>(R.id.textViewUrl)
-        val textViewRefArticle = findViewById<TextView>(R.id.textViewRefArticle)
-        val textViewTitleArticle = findViewById<TextView>(R.id.textViewTitleArticle)
-        val ratingBar = findViewById<RatingBar>(R.id.ratingBar)
-        val imageButtonUrl = findViewById<ImageButton>(R.id.imageButtonUrl)
-        val imageButtonEdit = findViewById<ImageButton>(R.id.imageButtonEdit)
-        val checkBox = findViewById<CheckBox>(R.id.checkBox)
-        val article = ArticleRepository.getArticle(3)
-        if(article != null){
-            textViewDate.text = article.dateAchat.toString()
-            textViewPrice.text = "${article.prix}€"
-            textViewUrl.text = article.url
-            textViewRefArticle.text = article.description
-            textViewTitleArticle.text = article.intitule
-            ratingBar.rating = article.niveau.toFloat()
-            checkBox.isChecked = article.achete ?: false
-            imageButtonUrl.setOnClickListener {
+        //Je récupère l'instance du binding grâce à mon fichier layout XML
+        aavb = DataBindingUtil.setContentView(this,R.layout.activity_article_view)
+        //Dans AAVB je retrouver toutes les vues ET variables de mon fichiers Layout
+        aavb.article = ArticleRepository.getArticle(1)
+        if(aavb.article != null){
+             aavb.textViewDate.text = if(aavb.article?.dateAchat == null){
+                Date(1678877335000L).toString()
+            }else{
+                 aavb.article!!.dateAchat.toString()
+             }
+            aavb.imageButtonUrl.setOnClickListener {
                 Snackbar.make(it,
-                    article.url ?: "",
+                    aavb.article?.url ?: "",
                     1000).show()
             }
         }
--- mod4findviewdatabinding/src/main/java/com/example/mod4findviewdatabinding/MainActivity.kt
+++ mod4findviewdatabinding/src/main/java/com/example/mod4findviewdatabinding/MainActivity.kt
@@ -5,20 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.mod4findviewdatabinding.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var amb : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Affiche l'interface graphique layout activity_main.xml
-        setContentView(R.layout.activity_main)
+        amb = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
-        val button = findViewById<Button>(R.id.button)
-        textView.text = "HELLO WORLD 😊😊😊😂😂😂🤣🤣"
-        button.setBackgroundColor(Color.GREEN)
-        //
-        button.setOnClickListener {
+        val nasique = Singe("Nasique", "Jean-Michel",25,"Marron-Orange")
+        amb.singe = nasique
+        //val textView = findViewById<TextView>(R.id.textView)
+        //val button = findViewById<Button>(R.id.button)
+        amb.textViewHelloWorld.text = "HELLO WORLD 😊😊😊😂😂😂🤣🤣"
+        amb.button.setBackgroundColor(Color.GREEN)
+
+        amb.button.setOnClickListener {
             Snackbar.make(it,
                 "Hello vous avez push un bouton :D :D :D",
                 1000).show()
