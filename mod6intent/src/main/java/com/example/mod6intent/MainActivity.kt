1fe1784e546e8ae73d93b3d1ec116f1504957252
riderofzion <quentinrouet@hotmail.fr>
2023-03-17 09:48:39 +0100

    DEMO PERMISSION MOD 6

--- mod6intent/src/main/java/com/example/mod6intent/MainActivity.kt
+++ mod6intent/src/main/java/com/example/mod6intent/MainActivity.kt
@@ -5,8 +5,16 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()){ isGranted ->
+        val intent = Intent(
+            if(isGranted) Intent.ACTION_CALL else Intent.ACTION_VIEW,
+            Uri.parse("tel:+1(202)-418-5000"))
+        startActivity(intent)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +29,10 @@ class MainActivity : AppCompatActivity() {
                 Intent.ACTION_VIEW,
                 Uri.parse("geo:47.228589,-1.6178463"))
             startActivity(intent)
+        }
+        findViewById<Button>(R.id.buttonCallCIA).setOnClickListener {
+            //INTENT IMPLICITE = VIEW GEO
+            requestPermissionLauncher.launch(android.Manifest.permission.CALL_PHONE)
         }
     }
 }
